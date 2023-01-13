72093bae8314f319a59ab5ed3e69b856844b69cc
Mercy Wacera <waceraaamercy@gmail.com>
2023-01-13 15:42:32 +0300

    Added icon for the paybills

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    WelcomeComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatSlideToggleModule
+   
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
